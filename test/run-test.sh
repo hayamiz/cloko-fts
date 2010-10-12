72b6afe7e67c238448c32281aaaa55febb26283b
Yuto Hayamizu <y.hayamizu@gmail.com>
2010-10-13 06:48:42 +0900

    run-test.sh: GCC enabled

--- test/run-test.sh
+++ test/run-test.sh
@@ -11,4 +11,19 @@ if test -z "$CUTTER"; then
     CUTTER="`make -s -C $BASE_DIR echo-cutter`"
 fi
 
-$CUTTER -s $BASE_DIR "$@" $BASE_DIR
+CUTTER_ARGS=
+CUTTER_WRAPPER=
+if test x"$CUTTER_DEBUG" = x"yes"; then
+    CUTTER_WRAPPER="$top_dir/libtool --mode=execute gdb --args"
+    CUTTER_ARGS="--keep-opening-modules"
+elif test x"$CUTTER_CHECK_LEAK" = x"yes"; then
+    CUTTER_WRAPPER="$top_dir/libtool --mode=execute valgrind "
+    CUTTER_WRAPPER="$CUTTER_WRAPPER --leak-check=full --show-reachable=yes -v"
+    CUTTER_ARGS="--keep-opening-modules"
+fi
+
+CUTTER_ARGS="${CUTTER_ARGS} -s $BASE_DIR"
+
+LC_ALL=C
+export LC_ALL
+$CUTTER_WRAPPER $CUTTER $CUTTER_ARGS "$@" $BASE_DIR

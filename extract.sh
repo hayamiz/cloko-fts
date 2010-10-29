92bbbec7cadaf27326b6305d749aada307afba25
Yuto Hayamizu <y.hayamizu@gmail.com>
2010-10-29 11:02:11 +0900

    use parameter to choose document

--- extract.sh
+++ extract.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-EOD_LINE=$(cat /data/local2/haya/fts/cloko001.txt | grep -A 1000 $1 | grep -n EOD | head -1 | sed -e 's/:.*//g')
-cat /data/local2/haya/fts/cloko001.txt | grep -A 1000 $1 | head -${EOD_LINE}
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 FILE URL"
+    exit 1
+fi
+
+datafile=$1
+url=$2
+
+EOD_LINE=$(cat $datafile | grep -A 1000 $url | grep -n EOD | head -1 | sed -e 's/:.*//g')
+# echo "EOD_LINE = $EOD_LINE" > /dev/stderr
+cat $datafile | grep -A 1000 $url | head -${EOD_LINE}
 
